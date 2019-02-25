38c68ea7de7137a86e36919f6577f0eacbfbd6d3
Jackson Watkins <jacksonwatkins@Jacksons-MacBook-Pro-2.local>
2019-02-25 01:14:09 -0800

    Set up Problem3 with reading the .csv files for the obs and exp data

--- assignment2/model_validator.py
+++ assignment2/model_validator.py
@@ -10,3 +10,16 @@ Problem 3
 Attempts to validate observational model by comparing queries
 in both observational and experimental settings.
 '''
+
+import numpy as np
+import pandas as pd
+from sklearn import linear_model, preprocessing, tree
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, KBinsDiscretizer  # noqa: E501
+from sklearn.naive_bayes import GaussianNB
+
+
+COLUMNS = ["X", "Z", "Y", "M", "W"]  # noqa: E501
+
+# reading training and test data
+obs_data = pd.read_csv('med_ex_obs.csv', names=COLUMNS)
+exp_data = pd.read_csv('med_ex_exp.csv', names=COLUMNS)
